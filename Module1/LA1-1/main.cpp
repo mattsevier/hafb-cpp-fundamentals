7db275150fd4b1f3955900cfcec28b2ae9421e03
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:02 -0700

    Added input

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
+    int age;  // int: signed integers
+    // cout is for system output
     std::cout << "Hello World" << std::endl;
+    cout << "Second Line" << endl;
+    cout << "Wildcats "<<"WSU "<<"Ogden"<<endl;
+    // cin is for system/user input
+    cout << "What is your age?" <<endl;
+    cin >> age;
+    cout << "I see you are: " << age <<endl;
     return 0;
     }
 
