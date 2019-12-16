79a7538050f3d46a0a69bf7a940f7f3adae5b311
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 11:16:32 -0700

    Added global variables

--- Module1/LA1-1/BMI.cpp
+++ Module1/LA1-1/BMI.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+const float kMeterToInches = 39.37; //These are globally defined variables
+const float kKiloToPound = 2.204; //No magic numbers, i.e. only variables in the code
+const float kBMIImperial = 703;
+
 int main()
 {
     // Calculate BMI: weight (kg) / [height (m)]^2
@@ -10,8 +14,11 @@ int main()
     cin >> weight;
     cout << "Enter you height in meters" << endl; //You can delete the endl so the text is placed at the end of the input question
     cin >> height;
+    //convert units from metric
+    weight = weight * kKiloToPound; //convert to pounds
+    height = height * kMeterToInches; //convert to inches
     // Calculate BMI
-    BMI = weight/(height*height);
+    BMI = (weight*kBMIImperial)/(height*height); //BMI calculation in standard units
     cout << "Your BMI is: " << BMI << endl;
 
     return 0;
