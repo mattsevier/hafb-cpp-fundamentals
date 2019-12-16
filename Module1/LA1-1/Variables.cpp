d7e1b06d36e35a572a630e6fb8605a1c9358a723
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 10:38:15 -0700

    Initial Commit

---
+++ Module1/LA1-1/Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\nData type  bytes" 
+    << "\n----------------" 
+    << "\nint     "  << sizeof(int) 
+    << "\nchar    " << sizeof(char) 
+    << "\nlong    " <<sizeof(long) 
+    << "\nbool    " <<sizeof(bool)
+    <<endl;
+
+    return 0;
+}
