d5378d69033ec8a71a993d55667c9dd9d5116111
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 16:39:51 -0700

    end of day

--- Module1/TO1-1/main.cpp
+++ Module1/TO1-1/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include "factor.h"
+//#include "factor.h"
 using namespace std;
 
 // Function Prototype
+void FactorMod3();
 
 // Main Function
 int main()
 {
+  FactorMod3();
   return 0;
+}
+
+//Function Definition
+void FactorMod3()
+{
+unsigned int count = 0;
+  for(unsigned int x = 1; x <= 30; ++x)
+{
+   if (x % 3 == 0)
+{
+  count++;
+}
+}
+cout << "Result is: " << count << endl;
 }
