b4ec656d0e6818116e161742f4715a43bbf025cc
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-17 16:31:33 -0700

    working with example

--- Module2/LA2-1/src/main.cpp
+++ Module2/LA2-1/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std;
+#include <string> //for string objects
 
 /**
  * @brief Shows how to define and use a string object
@@ -12,24 +12,65 @@ using namespace std;
  */
 int main() {
   // create a variable of type string
-
+  std::string name = "Matt"; 
   // print it out
+  std::cout << name << std::endl;
 
-  // get a string from a user
+  // // get a string from a user
+  // std::cout << "enter your name" << std::endl;
+  // //std::cin >> name; //sees space as null character and stops reading into cin
+  // //to get strings with spaces use getline() instead of cin
+  // getline(std::cin, name);
+  // std::cout << "Hi " << name << std::endl;
 
   // add two strings together
+  std::string title = "Dr.";
+  std::string formal;
+  std::string space = " ";
+  formal = title + space + name;
+  //formal = title + " " + name; //works also
+  std::cout << "Hi: " << formal << std::endl;
 
   // how long is the string
+  std::cout << "the length of name is " << name.size() << std::endl;
+  std::cout << "the length of title is " << title.size() << std::endl;
+  std::cout << "the length of title is " << title.length() << std::endl;
+  std::cout << "the length of formal is " << formal.length() << std::endl;
 
   // use [] to get an individual character
+  std::cout << "the first character of name is " << name[0] << std::endl;
+  std::cout << "the third character of name is " << name[2] << std::endl;
 
   // get name and commute information from a user
+  int minutes, miles;
+  std::cout << "enter your name" << std::endl;
+  getline(std::cin, name);
+  std::cout << "How long is your commute in minutes" << std::endl;
+  std::cin >> minutes;
+  std::cout << "How many miles is your commute" << std::endl;
+  std::cin >> miles;
+  std::cout << name << " has a " << minutes << " minutes, " << miles <<
+  " miles commute" << std::endl;
+
+
 
   // get commute information and name from a user
 
   // the string function find
+  std::string story = "Now is the time for all good men to come to the aid of their country";
+  std::cout << "men is at location " << story.find("men") << std::endl;
+
+
 
   // the string function replace
+
+  std::cout << story << std::endl;
+  story.replace(29, 3, "xxxxx");//replace men with xxxxx
+  std::cout << story << std::endl;
+
+  //could use variables in this and combine last two activities or use to replace all magic numbers
+  
+ 
 
   return 0;
 }
--- Module2/LA2-2/src/main.cpp
+++ Module2/LA2-2/src/main.cpp
@@ -1,8 +1,9 @@
-#include <array>
-#include <iostream>
-#include <string>
+#include <array>  //array containers
+#include <iostream> //basic I/O
+#include <string>  //strings
+#include <fstream> //file stream
 
-using namespace std;
+//using namespace std;
 
 /**
  * @brief Shows how to define and use an ifstream object to read from a file.
@@ -12,20 +13,79 @@ using namespace std;
  *
  * @return int 0 means function exited with success
  */
+
+const int kMaxSize = 24;
+
 int main() {
   // create an ojbect of type ifstream
 
+  std::ifstream in; //input file stream
+
+
   // open the file age.txt
+
+  in.open("../age.txt"); //relative location from the executable file, dotdot is up a file
+  if(in.fail())//did the file open?
+  {
+    std::cout << "unable to open file\n shutting down\n";
+    return 1;
+  }
 
 
   // read from the age file and store values in an array
 
-
-  // print out the values in the array
-
+  std::array<int, kMaxSize> ages;
+  ages.fill(-1); //initialize elements of the array to -1
+  int age_size = 0;
+  //in >> ages[age_size];
 
   // read to the end of the file
 
+  while(age_size < kMaxSize && in >> ages[age_size])
+  {
+    age_size++;
+  }
+  
+
+
+  // print out the values in the array
+  for(auto age: ages)
+  {
+    std::cout << age << std::endl;
+  }
+
+in.close();//close input file, always good practice
+
+
+std::ifstream infile; //input file stream
+
+ infile.open("../name.txt"); //relative location from the executable file, dotdot is up a file
+  if(infile.fail())//did the file open?
+  {
+    std::cout << "unable to open file\n shutting down\n";
+    return 1;
+  }
+  
+  std::array<std::string, kMaxSize> names;
+  names.fill("N/A"); //initialize elements of the array to -1
+  int name_size = 0;
+
+  
+  while(name_size < kMaxSize && std::getline(infile, names[name_size]))
+  {
+    name_size++;
+  }
+
+   // print out the values in the array
+  for(auto name: names)
+  {
+    std::cout << name << std::endl;
+  }
+
+infile.close();//close input file, always good practice
+
+
+  
 
   // print out the valid values in the array
 
@@ -55,6 +115,53 @@ int main() {
 
 
   // read all the values from the name_age.txt file
+
+    std::ifstream NaA; //input file stream
+
+
+  // open the file age.txt
+
+  NaA.open("../name_age.txt"); //relative location from the executable file, dotdot is up a file
+  if(NaA.fail())//did the file open?
+  {
+    std::cout << " unable to open file\n shutting down\n";
+    return 1;
+  }
+
+
+  // read from the age file and store values in an array
+
+  //std::array<int, kMaxSize> ages;
+  ages.fill(-1); //initialize elements of the array to -1
+  
+  //in >> ages[age_size];
+  //std::array<std::string, kMaxSize> names;
+  names.fill("N/A"); //initialize elements of the array to -1
+
+  // read to the end of the file
+  int size = 0;
+   while(size < kMaxSize && NaA >> ages[size])
+  {
+    //need to load the next record
+    //any extra characters?  yes
+    NaA.ignore();
+    std::getline(NaA, names[size]);
+    size++;
+  }
+
+  
+  
+
+
+  // print out the values in the array
+
+
+  for(int num = 0; num < size; ++num)
+  {
+    std::cout << ages[num] << " for " << names[num] << std::endl;
+  }
+
+NaA.close();//close input file, always good practice
 
 
   // print valid elements in the names and ages array
