f2afe575614132564b310dc031d4d35e0418a124
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-17 13:56:17 -0700

    Working

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower() //nameless namespace
+{
+    return 150;
+}
+
 // using namespace lamborghini;
+
+int lamborghini::HorsePower()
+{
+    return 760;
+}
+
 // using namespace porsche;
+
+int porsche::HorsePower()
+{
+    return 700;
+}
--- Module1/TO1-5/src/cars.h
+++ Module1/TO1-5/src/cars.h
@@ -1,5 +1,16 @@
 #ifndef CARS_H_
 #define CARS_H_
 
+int HorsePower();//nameless namespace (use :: to call)
+
+namespace lamborghini
+{
+    int HorsePower();//inside lamborghini namespace
+}
+
+namespace porsche
+{
+    int HorsePower();//inside Porsche namespace
+}
 
 #endif /* !CARS_H_ */
