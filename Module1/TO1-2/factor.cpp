64b2a52b3c52a8d4a3e4e2f16e3c0e6394385d41
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-17 09:07:33 -0700

    now working

--- Module1/TO1-2/factor.cpp
+++ Module1/TO1-2/factor.cpp
@@ -1,1 +1,22 @@
 #include "factor.h"
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Calculate the number of instances divisible by 3 in the range 1 through 30
+ * 
+ */
+
+void FactorMod3()
+{
+    unsigned int count = 0;
+    for(unsigned int x = 1; x <= 30; ++x)
+    {
+        if (x % 3 == 0)
+        {
+            count++;
+        }
+    }
+        cout << "Result is: " << count << endl;
+    
+} 
--- Module1/TO1-2/factor.h
+++ Module1/TO1-2/factor.h
@@ -1,5 +1,10 @@
 #ifndef FACTOR_H_
 #define FACTOR_H_
 
+//can use #pragma once if that has been loaded
+
+//Prototypes
+void FactorMod3();
+
 
 #endif /* !FACTOR_H_ */
