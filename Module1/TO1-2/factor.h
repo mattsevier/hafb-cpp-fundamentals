e021c3e372b5a8e002b8d7aef02ef3185a14a84e
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-17 09:49:25 -0700

    Now sends info in

--- Module1/TO1-2/factor.h
+++ Module1/TO1-2/factor.h
@@ -6,5 +6,10 @@
 //Prototypes
 void FactorMod3();
 
+//Added another requirement for flexible input, not hard coded
+void FactorModX(int mod_number); //we are now passing something in
+
+void FactorModXRange(int mod_number, int max_range);
+
 
 #endif /* !FACTOR_H_ */
