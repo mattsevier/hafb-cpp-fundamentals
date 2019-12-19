48fe8fb1d6ccf7964a6f3c3ba56a5388fe34336a
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-19 11:08:06 -0700

    Not totally complete

--- Module3/LA3-4/src/main.cpp
+++ Module3/LA3-4/src/main.cpp
@@ -5,6 +5,30 @@ using namespace std;
 // Main Function
 int main() 
 {
+  Distance d1(10,6);
+  d1.ShowDist();
+
+  Distance d2(1,6);
+  Distance d3;
+  d2.ShowDist();
+
+  d3 = d1 + d2;
+  d3.ShowDist();
+
+  Distance d4;
+  d4 = d1 + d2 + d3;
+  d4.ShowDist();
+
+  std::cout << d4 << std::endl;
+
+  // Distance d5;
+  // d5 = d1 - d2;
+  // d5.ShowDist();
+
+  // if(d1 < d2)
+  // {
+  //   std::cout << "They are the same distance" << std::endl;
+  // }
 
   return 0;
 }
