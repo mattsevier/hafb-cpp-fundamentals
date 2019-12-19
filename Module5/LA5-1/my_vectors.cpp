0735dbe8caa69e9bc7388a87d3c10678f686ab6a
Matthew Sevier <matthewsevier@ad.weber.edu>
2019-12-19 15:06:56 -0700

    stuff and things

--- Module5/LA5-1/my_vectors.cpp
+++ Module5/LA5-1/my_vectors.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
+//using namespace std;
 
 int main()
 {
+    std::vector<int> vec;
+    //add elements
+    vec.push_back(100); //both front() and back() contain the 100 value
+    vec.push_back(200); //front() =100 and back() = 200
+    std::cout << "Front of vector: " << vec.front() << std::endl;
+    std::cout << "Back of vector: " << vec.back() << std::endl;
+    vec.push_back(300);
+    //iterate over the vector
+    std::cout << "Using index notation " << std::endl;
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << std::endl;
+    }
+
+    std::cout << "Using an iterator" << std::endl;
+    for (auto it = std::begin(vec); it != std::end(vec); ++it)
+    {
+        std::cout << *it << std::endl; //must de-reference "it" to get the value "it" points to 
+    }
+
+    std::cout << "Using range based loop" << std::endl;
+    for (const auto& value: vec)
+    {
+        std::cout << value << " ";
+    }
+    std::cout<<std::endl;
+
+    //define and initialize vector, use { } with comma separated values
+    std::vector<int> vec2 = {-1,-1,-1,-1,-1};
+
+    for (int x : vec2)
+    {
+        std::cout << x << std::endl;
+    }
+
+    int max = 5;
+     std::vector<int> vec3(max, 4);//vector constructor
+
+    for (int xx : vec3)
+    {
+        std::cout << xx << " ";
+    }
+    std::cout<<std::endl;
 
     return 0;
 }
--- Module5/LA5-2/src/my_sets.cpp
+++ Module5/LA5-2/src/my_sets.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <set>
 
+//sets: elements are sorted according to the values
+//elements are all distinct value
+// a container with a UNIQUE group sorted elements, only keeps one copy, no duplicates
+//methods: size, empty, insert, erase
+//find() returns the position of the first element equal to the provided value
+        //you should always compare the value against the end of the container
+        //it always returns a value, and if it can't find one it returns the end
+
 int main()
 {
+
+    std::set<int> numbers;
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(30);
+
+    //search for a value
+    if(numbers.find(10) != numbers.end())//end() is not the last element but an extra byte at the end
+    {
+        std::cout << "10 is in numbers" << std::endl;
+    }
+
+    std::cout << "the set has " << numbers.size() << " elements" << std::endl;
+
+    std::cout << "Iterators" << std::endl;
+    for(auto it = std::begin(numbers); it != std::end(numbers); ++it)
+    {
+        std::cout<< *it << std::endl;
+    }
+
+    std::cout << "range based" << std::endl;
+    for(const auto& value: numbers)
+    {
+        std::cout<< value << std::endl;
+    }
+
+    //or 
+
+     std::cout << "also range based" << std::endl;
+    for(const auto& value: numbers)
+    {
+        std::cout<< value << " ";
+    }
+    std::cout << std::endl;
 
 }
